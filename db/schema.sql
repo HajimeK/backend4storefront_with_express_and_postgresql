e259ba7ed2417ad58eeedcf9d240e764dfe6e06f
HajimeK <smec.kawata@gmail.com>
2021-09-23 00:53:56 -0700

    docker up

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
 \set my_pass `echo "$POSTGRES_TABLE_PASSWORD"`
 CREATE EXTENSION PGCRYPTO;
 CREATE DATABASE backend4storefront;
+CREATE DATABASE backend4storefront_dev;
+CREATE DATABASE backend4storefront_test;
