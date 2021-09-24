b49486262cdda345dc04c9a1af3e4c11e5fa08be
HajimeK <smec.kawata@gmail.com>
2021-09-23 19:41:06 -0700

    fix postgres continuously restarts

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,3 @@
-\set my_pass `echo "$POSTGRES_TABLE_PASSWORD"`
-CREATE EXTENSION PGCRYPTO;
 CREATE DATABASE backend4storefront;
 CREATE DATABASE backend4storefront_dev;
 CREATE DATABASE backend4storefront_test;
